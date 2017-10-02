4bfb056e3a4d130be8b798c06327dcc22edebdaf
Vitaly <yudin5@yandex.ru>
2017-10-02 16:27:27 +0400

    Add files via upload

---
+++ task12/task1226/Solution.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task12.task1226;
+
+/* 
+Лазать, летать и бегать
+*/
+
+public class Solution {
+
+    public interface Fly {
+        void fly();
+    }
+
+    public interface Climb {
+        void climb();
+    }
+
+    public interface Run {
+        void run();
+    }
+
+    public class Cat implements Climb, Run {
+        public void climb() {};
+        public void run() {};
+    }
+
+    public class Dog implements Run {
+        public void run() {};
+    }
+
+    public class Tiger extends Cat {
+        public void climb() {};
+        public void run() {};
+    }
+
+    public class Duck implements Fly, Run{
+        public void fly() {};
+        public void run(){};
+    }
+}
---
+++ task12/task1229/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.task.task12.task1229;
+
+/* 
+Родитель класса CTO
+*/
+
+import java.awt.image.BufferStrategy;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+
+    public static class IT implements Businessman {
+        public void workHard() {};
+    }
+
+    public static class CTO extends IT implements Businessman {
+
+    }
+}
